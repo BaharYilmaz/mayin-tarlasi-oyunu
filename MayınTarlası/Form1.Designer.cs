b6333d272a9570bc245891ae9278ed37c02b7852
Bahar Yılmaz <baharr.yilmaz48@gmail.com>
2019-05-01 13:46:42 +0300

    first commit

---
+++ MayınTarlası/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace MayınTarlası
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUret = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.lbl_skor = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnUret
+            // 
+            this.btnUret.Location = new System.Drawing.Point(527, 35);
+            this.btnUret.Name = "btnUret";
+            this.btnUret.Size = new System.Drawing.Size(75, 83);
+            this.btnUret.TabIndex = 0;
+            this.btnUret.Text = "Uret";
+            this.btnUret.UseVisualStyleBackColor = true;
+            this.btnUret.Click += new System.EventHandler(this.btnUret_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(497, 344);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // lbl_skor
+            // 
+            this.lbl_skor.AutoSize = true;
+            this.lbl_skor.Location = new System.Drawing.Point(524, 180);
+            this.lbl_skor.Name = "lbl_skor";
+            this.lbl_skor.Size = new System.Drawing.Size(16, 17);
+            this.lbl_skor.TabIndex = 2;
+            this.lbl_skor.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(524, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Skor:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 408);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbl_skor);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.btnUret);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUret;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label lbl_skor;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MayınTarlası/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MayınTarlası
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUret_Click(object sender, EventArgs e)
+        {
+            int mayin1 = 0, mayin2 = 0, mayin3 = 0;
+            Random rnd = new Random();
+            mayin1 = rnd.Next(1, 15);
+            mayin2 = rnd.Next(16, 35);
+            mayin3 = rnd.Next(36, 50);
+
+            for (int i = 1; i <= 50; i++)
+            {
+                Button btntemp = new Button();
+                btntemp.Name = "btn" + i.ToString();
+                btntemp.Size = new System.Drawing.Size(35, 35);
+                btntemp.Text = i.ToString();
+                btntemp.UseVisualStyleBackColor = true;
+                if(mayin1==i||mayin2==i||mayin3==i)
+                {
+                    btntemp.Tag = true;
+
+                }
+                else
+                {
+                    btntemp.Tag = false;
+                }
+                
+                btntemp.Click += Btntemp_Click;
+                flowLayoutPanel1.Controls.Add(btntemp);
+
+            }
+
+        }
+
+        private void Btntemp_Click(object sender, EventArgs e)
+        {
+            Button basilanButton = ((Button)sender);
+            bool mayin =(bool)basilanButton.Tag;
+            if (mayin)
+            {
+                MessageBox.Show("Mayin bulundu");
+                basilanButton.BackColor = Color.Red;
+                int mayinInt=0;
+                mayinInt++;
+                if (mayinInt==3)
+                {
+                    MessageBox.Show("oyun bitti");
+                    
+                }
+
+            }
+            else
+            {
+                basilanButton.BackColor = Color.Green;
+                int skor = int.Parse(lbl_skor.Text);
+                skor++;
+                lbl_skor.Text = skor.ToString();
+
+            }
+        }
+    }
+}
